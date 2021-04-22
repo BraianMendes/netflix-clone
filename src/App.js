863f1a967773f5efddb5fb9ea449eb76dbfb6112
Braian Corrêa <00.braian.dev@gmail.com>
2021-04-22 04:08:44 -0300

    [Feat] Initialize Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
 import { Home } from './pages';
-
 
 export function App() {
   return (
-    <Home />
-    );
+    <Router>
+      <Switch>
+        <Home path={ROUTES.HOME}/>
+      </Switch>
+    </Router>
+  );
 }
