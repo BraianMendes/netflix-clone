4b62509691a1de6f746d0b95770f2bb640d64c7f
Braian Corrêa <00.braian.dev@gmail.com>
2021-04-22 05:43:01 -0300

    [Feat] Finished Profile Container and Component, Working Browser Page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'normalize.css';
 import { GlobalStyles } from './global-styles';
-import { firebase } from './libs/firebase.prod';
-import { FirebaseContext } from './context/firebase';
 import { App } from './app';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
 render(
   <React.StrictMode>
